4bb3a5a0f1226aaa2df1cdb5318f1067dc2fa557
Ben <ben.s.pritchard@gmail.com>
2015-03-27 16:37:05 -0700

    Add one more test and readme.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ describe User do
   it { should validate_presence_of :password }
   it { should have_many :posts }
   it { should have_many :comments }
+  it "sends an email when the user is created" do
+    user = FactoryGirl.create(:user)
+    ActionMailer::Base.deliveries.last.to.should eq [user.email]
+  end
 end
